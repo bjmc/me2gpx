f338ccb8c60c72e0c0b6626fa29b5bbea5f06a29
Brendan McCollam <bmccollam@uchicago.edu>
2017-05-25 21:43:33 +0100

    tabs->spaces

--- me2gpx/medat.py
+++ me2gpx/medat.py
@@ -10,33 +10,32 @@ POINT_PACK_SIZE = struct.calcsize(POINT_PATTERN)
 HEADER_SIZE = 4
 
 class PointSource(Enum):
-	EAppleNative=0
-	EWindowsNative=1
-	EAndroidNative=2
-	EGoogle=3
-	ETizen=4
-	EPredictor=5
+    EAppleNative=0
+    EWindowsNative=1
+    EAndroidNative=2
+    EGoogle=3
+    ETizen=4
+    EPredictor=5
 
 Point = namedtuple("Point", ('timestamp',
-							 'latitude',
-							 'longitude',
-							 'altitude',
-							 'speed',
-							 'bearing',
-							 'horizontalAccuracy',
-							 'verticalAccuracy',
-							 'source')
-
+                             'latitude',
+                             'longitude',
+                             'altitude',
+                             'speed',
+                             'bearing',
+                             'horizontalAccuracy',
+                             'verticalAccuracy',
+                             'source')
 		)
 
 def make_point(raw_point):
-	return Point(*raw_point[:-1], source=PointSource(raw_point[-1]))
+    return Point(*raw_point[:-1], source=PointSource(raw_point[-1]))
 
 
 def load_from_dat_file(filehandle):
-	data_input = filehandle.read()
-	data_len = len(data_input) - HEADER_SIZE
-	start = HEADER_SIZE
-	end = HEADER_SIZE + (POINT_PACK_SIZE *data_len) // POINT_PACK_SIZE
-	raw_points = data_input[start:end]
-	return map(make_point, struct.iter_unpack(POINT_PATTERN, raw_points))
+    data_input = filehandle.read()
+    data_len = len(data_input) - HEADER_SIZE
+    start = HEADER_SIZE
+    end = HEADER_SIZE + (POINT_PACK_SIZE *data_len) // POINT_PACK_SIZE
+    raw_points = data_input[start:end]
+    return map(make_point, struct.iter_unpack(POINT_PATTERN, raw_points))
