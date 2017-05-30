b1e7e3db81020037b69d9be4ead86d0327aef69c
Brendan McCollam <bmccollam@uchicago.edu>
2017-05-30 13:24:17 +0100

    Comment on env.py

--- test/env.py
+++ test/env.py
@@ -1,6 +1,7 @@
 import sys
 from os import path
-
+# Add me2gpx to python path so tests
+# can import the code they need to exercise.
 package_path = path.abspath(
                     path.dirname(
                         path.dirname(__file__)
